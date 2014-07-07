d38d13387c01c8dfeb6d318dbcc6f247c69ae56a
Irina Gaynanova <ig93@cornell.edu>
2014-07-07 00:00:00 +0000

    version 1.1

--- R/classifyV.R
+++ R/classifyV.R
@@ -6,27 +6,25 @@
 # Xtest is n2 by p
 # Ytrain is n1 by 1
 # V is p by g-1
-
-#V should be standardized so that V^tWV=I
-
-classifyV<-function(Xtrain,Ytrain,Xtest,V,prior=T){
+classifyV<-function(Xtrain,Ytrain,Xtest,V,prior=T,tol1=1e-10){
   p=ncol(Xtrain)
   if (ncol(Xtest)!=p){
     stop("Dimensions of Xtrain and Xtest don't match!")
   }
+  if (any(is.na(Xtest))|any(is.na(Ytrain))|any(is.na(Xtrain))){
+      stop("Missing values are not allowed!")
+  }
   G=max(Ytrain)
   if (length(V)/(G-1)!=p){
     stop("Dimensions of Xtrain and V don't match!")
-  }
-  
+  }  
   ntrain=nrow(Xtrain)
   if (length(Ytrain)!=ntrain){
     stop("Dimensions of Xtrain and Ytrain don't match!")
   }
   
   ntest=nrow(Xtest)
-  Ytest=rep(0,ntest)
-  
+  Ytest=rep(0,ntest) 
   V=as.matrix(V)
 
   if (G==2){
@@ -39,32 +37,31 @@ classifyV<-function(Xtrain,Ytrain,Xtest,V,prior=T){
     }  
     Dis=matrix(testproj^2,ntest,2)-2*tcrossprod(testproj,means)+matrix(t(means^2),ntest,2,byrow=T)
     if (prior) Dis=Dis-matrix(2*log(c(sum(Ytrain==1),sum(Ytrain==2))/ntrain),ntest,2,byrow=T)
+    Ytest=apply(Dis,1,which.min)   
+    return(Ytest)
   } else{
-    ######################################
     ######### G>2 ########################   
-    A1=t(V)%*%t(Xtrain)%*%.constructCw(Ytrain)%*%Xtrain%*%V 
-    tmp=eigen(A1,symmetric=T)
-    if (min(tmp$values)>0){ V=V%*%tmp$vectors%*%diag(1/sqrt(tmp$values))
-    }else { # V is low rank
-      #return(rep(0,ntest))
-      V=V%*%tmp$vectors[,tmp$values>0]%*%diag(1/sqrt(tmp$values[tmp$values>0]))
-    }
-    
     trainproj=Xtrain%*%V
     testproj=Xtest%*%V
-    means=matrix(0,G,ncol(V))
-    ngroup=rep(0,G)
-    for (i in 1:G){
-      ngroup[i]=sum(Ytrain==i)
-      if (ngroup[i]>1) means[i,]=colMeans(trainproj[Ytrain==i,])
-      else means[i,]=trainproj[Ytrain==i,]
+    myg=as.factor(Ytrain)
+    group.means=tapply(trainproj,list(rep(myg,ncol(V)),col(trainproj)),mean)
+    A1=var(trainproj-group.means[myg,]) 
+    tmp=eigen(A1,symmetric=T)
+    if (min(tmp$values)>tol1){ V=V%*%tmp$vectors%*%diag(1/sqrt(tmp$values))
+    }else { # V is low rank
+        if (sum(tmp$values>tol1)>1){V=V%*%tmp$vectors[,tmp$values>tol1]%*%diag(1/sqrt(tmp$values[tmp$values>tol1]))
+        }else {V=V%*%tmp$vectors[,tmp$values==max(tmp$values)]/sqrt(tmp$values[tmp$values==max(tmp$values)])}
     }
-    
-    #currently have A as I
-    Dis=matrix(rowSums(testproj^2),ntest,G)-2*tcrossprod(testproj,means)+matrix(rowSums(means^2),ntest,G,byrow=T)
-    if (prior) Dis=Dis-matrix(2*log(ngroup/ntrain),ntest,G,byrow=T)
+        
+    trainproj=Xtrain%*%V
+    testproj=Xtest%*%V
+
+    if (prior==T){
+        outlda=lda(trainproj,grouping=Ytrain,tol=1e-16)
+    } else{
+        outlda=lda(trainproj,grouping=Ytrain,prior=rep(1/max(Ytrain),max(Ytrain)),tol=1e-16)
+    }
+    ypredlda=predict(outlda,testproj)
+    return(ypredlda$class)
   }
-  
-  Ytest=apply(Dis,1,which.min)   
-  return(Ytest)
 }
--- R/cv.dLDA.R
+++ R/cv.dLDA.R
@@ -1,4 +1,4 @@
-cv.dLDA<-function(Xtrain,Ytrain,lambdaval=NULL,nl=100,msep=5,eps=1e-6,l_min_ratio=0.01){
+cv.dLDA<-function(Xtrain,Ytrain,lambdaval=NULL,nl=100,msep=5,eps=1e-6,l_min_ratio=0.01,myseed=NULL,prior=TRUE){
   if (any(is.na(Xtrain))|any(is.na(Ytrain))) 
     stop("Missing values are not allowed!")
   
@@ -10,15 +10,12 @@ cv.dLDA<-function(Xtrain,Ytrain,lambdaval=NULL,nl=100,msep=5,eps=1e-6,l_min_rati
   p=ncol(Xtrain)
   
   if (G==2){
-    # two-group case, use glmnet
     n1=sum(Ytrain==1)
     n2=sum(Ytrain==2)
     Ynew=Ytrain
     Ynew[Ytrain==1]=-n/n1
     Ynew[Ytrain==2]=n/n2
-    
-    #require(glmnet)
-    
+
     #calculate lambda path
     if (is.null(lambdaval)){
       out=glmnet(Xtrain,Ynew,family="gaussian",lambda=lambdaval,alpha=1,standardize=T)
@@ -26,48 +23,41 @@ cv.dLDA<-function(Xtrain,Ytrain,lambdaval=NULL,nl=100,msep=5,eps=1e-6,l_min_rati
     }
   
     nl=length(lambdaval)
-    error=matrix(0,msep,nl) #msep by lambda[i]
+    error=matrix(0,msep,nl)
     features=matrix(p,msep,nl)
     
-    #split the dataset into msep parts for each type
+    if (!is.null(myseed)){set.seed(myseed)}
     id=1:n
     for (i in 1:G){
       id[Ytrain==i] <- sample(rep(seq_len(msep),length.out=sum(Ytrain==i)))
     }
     
     cat("Fold")
-    #for each split of the dataset
     for (i in 1:msep){   
       cat(i)
-      # determine test set and train set
       xtrain=Xtrain[id!=i,]
       ytrain=Ynew[id!=i]
       xtest=Xtrain[id==i,]
       ytest=Ytrain[id==i]
       
-      #call glmnet once for all those lambda
       out=glmnet(xtrain,ytrain,family='gaussian',alpha=1,lambda=lambdaval,standardize=T)
       features[i,]=out$df
       
-      #calculate the error rate
       for (j in 1:nl){  
-       ypred=classifyV(xtrain,Ytrain[id!=i],xtest,out$beta[,j])
+       ypred=classifyV(xtrain,Ytrain[id!=i],xtest,out$beta[,j],prior=prior)
        error[i,j]=sum(ypred!=ytest) #how many features are selected
       }
     }
-    #calculate mean error for all lambda
     errormean=colMeans(error)
     j=which.min(errormean)
     obj<-list(lambda=lambdaval[j],error=colMeans(error),f=round(colMeans(features)),lambdaval=lambdaval)
     return(obj) 
   } else{
     #multiple group case
-    #calculate l_max
     D=.constructD(scale(Xtrain),Ytrain)
     l_max=max(sqrt(rowSums(D^2)))
     rm(D)
     
-    #calculate lambda path
     if (!is.null(lambdaval)){
       lambdaval=lambdaval[lambdaval<=l_max]
       nl=length(lambdaval)
@@ -78,53 +68,46 @@ cv.dLDA<-function(Xtrain,Ytrain,lambdaval=NULL,nl=100,msep=5,eps=1e-6,l_min_rati
     }
     lambdaval=sort(lambdaval,decreasing=T)
     
-    #keep errors and features
-    error=matrix(0,msep,nl) #msep by lambda[i]
+    error=matrix(0,msep,nl) 
     features=matrix(p,msep,nl)
   
-    #split the dataset into msep parts for each type
+    if (!is.null(myseed)){set.seed(myseed)}
     id=1:n
     for (i in 1:G){
       id[Ytrain==i] <- sample(rep(seq_len(msep),length.out=sum(Ytrain==i)))
     }
   
     cat("Fold")
-    #for each split of the dataset
     for (i in 1:msep){   
       cat(i)
-      # determine test set and train set
       xtrain=Xtrain[id!=i,]
       Xadj=scale(xtrain)
       coef=attr(Xadj,which="scaled:scale")
-    
+      mtrain=attr(Xadj,which="scaled:center")
       ytrain=Ytrain[id!=i]
       xtest=Xtrain[id==i,]
+      xtest=scale(xtest,center=mtrain,scale=coef) 
       ytest=Ytrain[id==i]
       
-      #calculate D only once for all those lambdaval
       D=.constructD(Xadj,ytrain)
       Tot=crossprod(Xadj)/(length(ytrain)-1)
       V=matrix(0,p,G-1)
       error[i,1:nl]=length(ytest)
     
-      #make discrimination with lambda[j],calculate the error rate
       for (j in 1:nl){  
-        # no scaling for initial value
         V=.solveVcoordf2(Tot,D,lambdaval[j],eps=eps,V=V)
-   
-        features[i,j]=sum(rowSums(V)!=0) #how many features are selected
+        features[i,j]=sum(rowSums(V)!=0) 
       
-        if (features[i,j]>p-1){ #get to the end of the path when all the features are selected
-          ytestpred=classifyV(xtrain,ytrain,xtest,diag(1/coef)%*%V)
+        if (features[i,j]>p-1){
+          ytestpred=classifyV(Xadj,ytrain,xtest,V,prior=prior)
           error[i,j:nl]=sum(ytestpred!=ytest)
           break
-        }else if (features[i,j]>G-1){
-          ytestpred=classifyV(xtrain,ytrain,xtest,diag(1/coef)%*%V)
+        }else if (features[i,j]>0){
+          ytestpred=classifyV(Xadj,ytrain,xtest,V,prior=prior)
           error[i,j]=sum(ytestpred!=ytest)
        }
     }
   }
-  #calculate mean error for all lambda
   errormean=colMeans(error)
   j=which.min(errormean)
   obj<-list(lambda=lambdaval[j],error=colMeans(error),f=round(colMeans(features)),lambdaval=lambdaval)
