28df0fe47b3f3f12f6d45f8deb01da039648f520
Irina Gaynanova <irinag@stat.tamu.edu>
2015-09-02 01:26:37 +0000

    version 1.2

--- R/cv.dLDA.R
+++ R/cv.dLDA.R
@@ -1,4 +1,4 @@
-cv.dLDA<-function(Xtrain,Ytrain,lambdaval=NULL,nl=100,msep=5,eps=1e-6,l_min_ratio=0.01,myseed=NULL,prior=TRUE){
+cv.dLDA<-function(Xtrain,Ytrain,lambdaval=NULL,nl=50,msep=5,eps=1e-6,l_min_ratio=0.01,myseed=NULL,prior=TRUE){
   if (any(is.na(Xtrain))|any(is.na(Ytrain))) 
     stop("Missing values are not allowed!")
   
@@ -9,43 +9,60 @@ cv.dLDA<-function(Xtrain,Ytrain,lambdaval=NULL,nl=100,msep=5,eps=1e-6,l_min_rati
   G=max(Ytrain)
   p=ncol(Xtrain)
   
-  if (G==2){
-    n1=sum(Ytrain==1)
-    n2=sum(Ytrain==2)
-    Ynew=Ytrain
-    Ynew[Ytrain==1]=-n/n1
-    Ynew[Ytrain==2]=n/n2
-
-    #calculate lambda path
-    if (is.null(lambdaval)){
-      out=glmnet(Xtrain,Ynew,family="gaussian",lambda=lambdaval,alpha=1,standardize=T)
-      lambdaval=out$lambda
-    }
+  if (!is.null(myseed)){set.seed(myseed)}
+  id=1:n
+  for (i in 1:G){
+      id[Ytrain==i] <- sample(rep(seq_len(msep),length.out=sum(Ytrain==i)))
+  }
   
-    nl=length(lambdaval)
+  if (G==2){
+      Z=matrix(0,n,2)
+      for (g in 1:2){
+          Z[Ytrain==g,g]=1
+      }
+      n1=sum(Ytrain==1)
+      n2=n-n1
+      Ytilde=sqrt(n1*n2)*Z%*%c(1/n1,-1/n2)
+
+     #calculate lambda path
+    l_max=max(abs(crossprod(scale(Xtrain),Ytilde)))/n
+    if (!is.null(lambdaval)){
+        lambdaval=lambdaval[lambdaval<=l_max]
+        nl=length(lambdaval)
+        if (nl<2) stop("There should be at least two lambdas")
+    }
+    else {
+        lambdaval=10^seq(log10(l_min_ratio*l_max),log10(l_max),length.out=nl)
+    }
+    lambdaval=sort(lambdaval,decreasing=T)
+  
     error=matrix(0,msep,nl)
     features=matrix(p,msep,nl)
-    
-    if (!is.null(myseed)){set.seed(myseed)}
-    id=1:n
-    for (i in 1:G){
-      id[Ytrain==i] <- sample(rep(seq_len(msep),length.out=sum(Ytrain==i)))
-    }
-    
+      
     cat("Fold")
     for (i in 1:msep){   
       cat(i)
       xtrain=Xtrain[id!=i,]
-      ytrain=Ynew[id!=i]
+      Xadj=scale(xtrain)
+      coef=attr(Xadj,which="scaled:scale")
+      mtrain=attr(Xadj,which="scaled:center")
+      ytrain=Ytrain[id!=i]
       xtest=Xtrain[id==i,]
+      xtest=scale(xtest,center=mtrain,scale=coef) 
       ytest=Ytrain[id==i]
       
-      out=glmnet(xtrain,ytrain,family='gaussian',alpha=1,lambda=lambdaval,standardize=T)
-      features[i,]=out$df
-      
+      nnew=length(ytrain)
+      Znew=Z[id!=i,]
+      n1=sum(ytrain==1)
+      n2=nnew-n1
+      Ytilde=sqrt(n1*n2)*Znew%*%c(1/n1,-1/n2)
+      V=rep(0,p)
+
       for (j in 1:nl){  
-       ypred=classifyV(xtrain,Ytrain[id!=i],xtest,out$beta[,j],prior=prior)
+        V=.solveMyLasso_c(Xadj,Ytilde,lambda=lambdaval[j],binit=V)
+       ypred=classifyV(Xadj,ytrain,xtest,V,prior=prior)
        error[i,j]=sum(ypred!=ytest) #how many features are selected
+       features[i,j]=sum(V!=0)
       }
     }
     errormean=colMeans(error)
@@ -54,28 +71,20 @@ cv.dLDA<-function(Xtrain,Ytrain,lambdaval=NULL,nl=100,msep=5,eps=1e-6,l_min_rati
     return(obj) 
   } else{
     #multiple group case
-    D=.constructD(scale(Xtrain),Ytrain)
-    l_max=max(sqrt(rowSums(D^2)))
-    rm(D)
-    
+    Ytilde=.createY(Ytrain)
+    l_max=max(abs(crossprod(scale(Xtrain),Ytilde)))/n
     if (!is.null(lambdaval)){
-      lambdaval=lambdaval[lambdaval<=l_max]
-      nl=length(lambdaval)
-      if (nl<2) stop("There should be at least two lambdas")
+        lambdaval=lambdaval[lambdaval<=l_max]
+        nl=length(lambdaval)
+        if (nl<2) stop("There should be at least two lambdas")
     }
     else {
-      lambdaval=10^seq(log10(l_min_ratio*l_max),log10(l_max),length.out=nl)
+        lambdaval=10^seq(log10(l_min_ratio*l_max),log10(l_max),length.out=nl)
     }
     lambdaval=sort(lambdaval,decreasing=T)
     
     error=matrix(0,msep,nl) 
     features=matrix(p,msep,nl)
-  
-    if (!is.null(myseed)){set.seed(myseed)}
-    id=1:n
-    for (i in 1:G){
-      id[Ytrain==i] <- sample(rep(seq_len(msep),length.out=sum(Ytrain==i)))
-    }
   
     cat("Fold")
     for (i in 1:msep){   
@@ -89,13 +98,12 @@ cv.dLDA<-function(Xtrain,Ytrain,lambdaval=NULL,nl=100,msep=5,eps=1e-6,l_min_rati
       xtest=scale(xtest,center=mtrain,scale=coef) 
       ytest=Ytrain[id==i]
       
-      D=.constructD(Xadj,ytrain)
-      Tot=crossprod(Xadj)/(length(ytrain)-1)
+      Ytilde=.createY(ytrain)
       V=matrix(0,p,G-1)
       error[i,1:nl]=length(ytest)
     
       for (j in 1:nl){  
-        V=.solveVcoordf2(Tot,D,lambdaval[j],eps=eps,V=V)
+        V=.solveMyLassoF_c(Xadj,Ytilde,lambda=lambdaval[j],binit=V)
         features[i,j]=sum(rowSums(V)!=0) 
       
         if (features[i,j]>p-1){
--- src/solveVcoordf2st.c
+++ src/solveVcoordf2st.c
@@ -2,11 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 
-//#define max(a,b)	       \
-//   ({ __typeof__ (a) _a = (a);		\
-//       __typeof__ (b) _b = (b);		\
-//     _a > _b ? _a : _b; })
+// new functions in C for MGSDA package
 
+//function for maximum
 double max(double a, double b){
     if (a>=b){
         return a;
@@ -15,111 +13,152 @@ double max(double a, double b){
     }
 }
 
-void mul(double *a, double *b, int* length,double* runningSum){
-   int index;
-   for (index = 0; index < *length; index++)
-     { *runningSum =*runningSum + a[index] * b[index];
-     }
+//function for inner product of two vectors
+double mul(double *a, double *b, int* length){
+    int index;
+    double result=0;
+    
+    for (index = 0; index < *length; index++){
+        result+=a[index] * b[index];
+    }
+    return result;
 }
 
-//here B is matrix and we want to multiply each a[i] by B[i,l]
-void mulV(double *a, double *B, int* length, int *l, double* runningSum){
+//////////////////////////////////////////
+// Set of functions for LASSO problem  ///
+//////////////////////////////////////////
+//function for residual
+void res(double *X, double *Y, double *beta, int *n, int *p, double *residual){
+    int j,k;
+    for (j=0; j<*n;j++){
+        residual[j]=Y[j];
+        for (k=0; k<*p; ++k){
+            residual[j]-=X[j+k*(*n)]*beta[k];
+        }
+    }
+}
+
+//function for calculating normx=colSums(x^2)/n
+void colNorm(double *X, int *n, int* p, double* normx){
   int index;
-  for (index=0; index<*length; index++)
-    { 
-      *runningSum=*runningSum+a[index]*B[index+(*l)*(*length)];
+  for (index=0; index<*p; index++){
+        normx[index]=mul(&X[index*(*n)],&X[index*(*n)],n)/(*n);
    }
 }
 
-//standardized version, W[k,k]=1
-void blockUpdate2(double *D, double *W,double *V, double *lambda, int *p, int *r, double *errV)
+//Function for one round of coordinate update
+void coordUpdateLasso(double *X,double *beta, double *lambda, int *n, int *p, double *errb,double *residual, double *normx)
 {
-  int k,l;
-  double normt;
+  int k,j;
   double colsum;
-  double tmp[*r]; //this is a vector of length r
-  double v_old;
+  double bold;
 
-  *errV=0;
+  *errb=0;
   for (k=0; k<*p; k++)
     {
-      //k=cursample[i]-1; //indexing in C starts from 0 so when I refer to k, need to adjust its number by 1
-      
-      for (l=0;l<*r;l++){
-	colsum=0;
-	mulV(&W[k*(*p)],V,p,&l,&colsum);//this multiplies W2[k,] by V[,l]
-	tmp[l]=D[k+l*(*p)]-colsum+V[k+l*(*p)];
-      }
-
-      normt=0;
-      mul(&tmp[0],&tmp[0],r,&normt); //this makes normt=sum(tmp^2)
-      normt=sqrt(normt);
-
-      if (normt<= *lambda){
-	for (l=0;l<*r;l++){
-	   v_old=V[k+l*(*p)];
-	   V[k+l*(*p)]=0; //V[i,j]=V[j*(*Nrow)+i]
-	   *errV=max(*errV,fabs(v_old-V[k+l*(*p)]));
-	}
-      }
-      else{
-	for (l=0;l<*r;l++){
-	  v_old=V[k+l*(*p)];
-	  V[k+l*(*p)]=(1-*lambda/normt)*tmp[l];
-	  *errV=max(*errV,fabs(v_old-V[k+l*(*p)]));
-	}
-      }
+      bold=beta[k];
+      colsum=mul(&X[k*(*n)],residual,n)/(*n)+normx[k]*bold;
+      beta[k]=((colsum>0)-(colsum<0))*max(0,fabs(colsum)-*lambda)/normx[k];
+        //update residual
+        for (j=0; j<*n;j++){
+            residual[j]+=(bold-beta[k])*X[j+k*(*n)];
+        }
+      *errb=max(*errb,fabs(bold-beta[k]));
     }
 }
 
-void coordUpdate(double *D, double *W,double *V, double *lambda, int *p, double *errV)
-{
-  int k;
-  double colsum;
-  double v_old;
+//Complete lasso solver
+void solveMyLasso(double *X, double *Y,double *beta, double *lambda, int *p, int *n, double *eps, int *maxiter,int *niter){
 
-  *errV=0;
-  for (k=0; k<*p; k++)
-    {
-      //k=cursample[i]-1; //indexing in C starts from 0 so when I refer to k, need to adjust its number by 1
+  double errb;
+  double residual[*n]; //this is a vector of length n
+  double normx[*p]; //this is a vector of length p
 
-      colsum=0;
-      mul(&W[k*(*p)],V,p,&colsum);//this multiplies W2[k,] by V[,l]
-      colsum=D[k]-colsum+V[k];
-      
-      v_old=V[k];
-      V[k]=((colsum>0)-(colsum<0))*max(0,fabs(colsum)-*lambda);
-      *errV=max(*errV,fabs(v_old-V[k]));
-    }
-}
-
-void solveVcoordf2(double *D, double *W,double *V, double *lambda, int *p, int *r,double *eps, int *maxiter,int *niter){
-
-  // int i;
-  double errV;
-  //int cursample[*p];
+    //calculate the current residual between y and Xbeta
+    res(X,Y,beta,n,p,&residual[0]);
+    //calculate l2 norm of each column of X standardized by n
+    colNorm(X,n,p,&normx[0]);
 
   *niter=0;
-  if(*r>1){
     do{
-      //for now do linear update each time
-      // this gives almost the same time as random update in R
-      //for (i=0;i<*p;i++){
-      //  cursample[i]=i+1;
-      // }
       ++*niter;
-      blockUpdate2(D,W,V,lambda,p,r,&errV);
-    }while ((errV>*eps)&&(*niter<*maxiter));
-  }
-  else{
+      coordUpdateLasso(X,beta,lambda,n,p,&errb,&residual[0],&normx[0]);
+    }while ((errb>*eps)&&(*niter<*maxiter));
+}
+
+//Function for one round of block update, here V is matrix
+void blockUpdateLasso(double *X,double *V, double *lambda, int *n, int *p, int *r, double *errV,double *residual, double *normx)
+{
+  int k,j,l;
+  double normt,v_old;
+  double T[*r];
+
+  *errV=0;
+  for (k=0; k<*p; k++)
+    {
+      //calculate T
+      for (l=0;l<*r;l++){ 
+        T[l]=mul(&X[k*(*n)],&residual[l*(*n)],n)/(*n)+normx[k]*V[k+l*(*p)];// be careful with residual indexing
+      }
+      
+      //calculate L_2 norm of T
+      normt=mul(&T[0],&T[0],r); //this makes normt=sum(T^2)
+      normt=sqrt(normt);
+      
+      //update kth row of V
+      if (normt<= *lambda){
+          for (l=0;l<*r;l++){
+              v_old=V[k+l*(*p)];
+              V[k+l*(*p)]=0; //V[i,j]=V[j*(*Nrow)+i]
+              //update residual
+              for (j=0; j<*n;j++){
+                residual[j+l*(*n)]+=(v_old-V[k+l*(*p)])*X[j+k*(*n)];
+              }   
+              *errV=max(*errV,fabs(v_old));
+          }
+      }
+      else{
+          for (l=0;l<*r;l++){
+              v_old=V[k+l*(*p)];
+              V[k+l*(*p)]=(1-*lambda/normt)*T[l]/normx[k];
+              //update residual
+              for (j=0; j<*n;j++){
+                residual[j+l*(*n)]+=(v_old-V[k+l*(*p)])*X[j+k*(*n)];
+              }  
+              *errV=max(*errV,fabs(v_old-V[k+l*(*p)]));
+          }
+      }
+    }
+}
+
+//function for residual that is a matrux
+void resF(double *X, double *Y, double *beta, int *n, int *p, int *r, double *residual){
+    int j,k,l;
+    for (j=0; j<*n;j++){
+        for (l=0; l<*r; ++l){
+            residual[j+l*(*n)]=Y[j+l*(*n)];
+            for (k=0; k<*p; ++k){
+                residual[j+l*(*n)]-=X[j+k*(*n)]*beta[k+l*(*p)];
+            }
+        }
+    }
+}
+
+//Complete lasso solver with Frobenius norm
+void solveMyLassoF(double *X, double *Y,double *beta, double *lambda, int *p, int *n, int *r, double *eps, int *maxiter,int *niter){
+
+  double errV;
+  double residual[(*n)*(*r)]; //this is a n times r matrix
+  double normx[*p]; //this is a vector of length p
+
+    //calculate the current residual between y and Xbeta
+    resF(X,Y,beta,n,p,r,&residual[0]);
+    //calculate l2 norm of each column of X standardized by n
+    colNorm(X,n,p,&normx[0]);
+
+  *niter=0;
     do{
-      //for now do linear update each time
-      // this gives almost the same time as random update in R
-      //for (i=0;i<*p;i++){
-      //  cursample[i]=i+1;
-      // }
       ++*niter;
-      coordUpdate(D,W,V,lambda,p,&errV);
+      blockUpdateLasso(X,beta,lambda,n,p,r,&errV,&residual[0],&normx[0]);
     }while ((errV>*eps)&&(*niter<*maxiter));
-  }
 }
